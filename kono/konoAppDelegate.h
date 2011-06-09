96fb9645902a3898384b39034881f1481d0d1072
Sammy Long <sammy@rescomp-10-197525.stanford.edu>
2011-06-08 19:39:11 -0700

    Initial Commit

---
+++ kono/konoAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  konoAppDelegate.h
+//  kono
+//
+//  Created by Sammy Long on 2011/6/8.
+//  Copyright 2011年 Stanford University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface konoAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ kono/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  kono
+//
+//  Created by Sammy Long on 2011/6/8.
+//  Copyright 2011年 Stanford University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
